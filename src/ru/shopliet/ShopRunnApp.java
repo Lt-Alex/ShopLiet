28b4720626e7f7cbd19db2a7a2ab18b10233edea
Lt-Alex <umnov_a@yahoo.com>
2022-03-15 20:29:44 +0300

    Commit1

--- src/ru/shopliet/ShopRunnApp.java
+++ src/ru/shopliet/ShopRunnApp.java
@@ -2,7 +2,7 @@ package ru.shopliet;
 
 public class ShopRunnApp {
     public static void main(String[] args) {
-        System.out.println("Shop Run1");
+        System.out.println("Shop Run");
 
     }
 }
